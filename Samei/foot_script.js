f2303412a98781f85fa3cc78189c8359e23298d0
Hananiel Setiawan <hananiel_s@live.com>
2018-05-16 23:23:36 -0400

    samei2

---
+++ Samei/foot_script.js
@@ -0,0 +1,23 @@
+document.getElementById("footerPage").innerHTML =
+	'<div class = "decorationfooter">'+
+'</div>'+
+	'<div class = "footer">'+
+	'<table width="68%" height="95%" border="0" align="center">'+
+  '<tbody>'+
+    '<tr>'+
+		'<td width="17%"><img src="image/duke_logo.png" width=110px alt="Duke Logo"/></td>'+
+      '<td width="33%"><pfoot>© Copyright 2018 Duke University</pfoot><br>'+
+        '<pfoot>PI: Ehsan Samei, PhD, DABR, FAAPM, FSPIE, FAIMBE</pfoot></td>'+
+      '<td width="34%">'+
+		'<pfoot>Carl E. Ravins Advanced Imaging Laboratory</pfoot><br>'+
+		'<pfoot>Hock Plaza, Suite 302</pfoot><br>'+
+		'<pfoot>2424 Erwin Road, Durham, NC 27705</pfoot><td width="0%"></td>'+
+      '<td width="16%">'+
+		'<pfoot><em>Phone</em>: +1 (919) 684-1440</pfoot><br>'+
+		'<pfoot><em>Fax</em>: +1 (919) 684-1491</pfoot><br>'+
+		'<pfoot><a href="mailto:robin.kearney@dm.duke.edu" target="_blank" rel="noopener">robin.kearney@dm.duke.edu</a></pfoot><td width="0%">'+
+			'</td>'+
+    '</tr>'+
+  '</tbody>'+
+'</table>'+
+'</div>';
