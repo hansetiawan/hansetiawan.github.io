b31d920c7209653ca64db1312e4fc2efe01cb2f1
Hananiel Setiawan <hananiel_s@live.com>
2018-07-16 17:13:22 -0400

    head script link Samei Group up top

--- Samei/head_script.js
+++ Samei/head_script.js
@@ -1,13 +1,13 @@
 document.getElementById("headerPage").innerHTML =
 '<header class = "primaryheader">'+
-	'<span>Samei Group</span>'+
+	'<span><a href="index.html" style="text-decoration: none; color: white;">Samei Group</a></span>'+
 		 '</header>'+
 
 '<header class = "secondaryheader">'+
  '<label for="show-menu" class="show-menu">Show Menu</label>'+
     '<input type="checkbox" id="show-menu" role="button">'+
         '<ul id="menu">'+
-        	'<li><a href="./">About</a></li>'+
+        	'<li><a href="index.html">About</a></li>'+
 			'<li>'+
 			'<a href="people.html">People</a>'+
 			'<ul class="hidden">'+
