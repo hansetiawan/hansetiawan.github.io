6bbfeb83a2382af0130c4577a1bd8337c85e1f57
Hananiel Setiawan <hananiel_s@live.com>
2018-07-17 10:28:15 -0400

    King text

--- Samei/slideshow.js
+++ Samei/slideshow.js
@@ -1,25 +1,26 @@
 document.getElementById("slideShow").innerHTML =
 '<div class="w3-content w3-display-container">'+
 
-'<div class="w3-display-container mySlides">'+
-  '<img src="image/action.png" style="width:100%">'+
-  '<div class="w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black">'+
-    'Bricks and Trees. Photo taken by someone professional,  defini tely not Han. Han doesnt own nice camera, just an old phone yes OK'+
-	'</div>'+
-'</div>'+
 
 '<div class="w3-display-container mySlides">'+
   '<img src="image/graduate.jpg" style="width:100%">'+
   '<div class="w3-display-bottomright w3-large w3-container w3-padding-16 w3-black">'+
-   'Graduates of the Medical Physics program. Dr. Marthony Robins is sitting right in the center, former member of the laboratory and a graduate. Just trying'+ 'to waste some space to see how this looks like for longer captions such as this. Should I write longer? I dont know but I will just stop right here.'+ 'Sufficient. Maybe. Or not? But that blue drape background is really something, isnt it?'+
+   'I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin but by the content of their character. (My Lord) I have a dream today.'+
   '</div>'+
 '</div>'+
 
 '<div class="w3-display-container mySlides">'+
-  '<img src="image/action.png" style="width:100%">'+
+  '<img src="image/graduate.jpg" style="width:100%">'+
   '<div class="w3-display-topleft w3-large w3-container w3-padding-16 w3-black">'+
-    'Beautiful Duke West Campus.'+
+    'Free at last! Free at last! Thank God Almighty, we are free at last!'+
   '</div>'+
+'</div>'+
+	
+'<div class="w3-display-container mySlides">'+
+  '<img src="image/action.png" style="width:100%">'+
+  '<div class="w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black">'+
+    'This is our hope. This is the faith that I go back to the South with.'+
+	'</div>'+
 '</div>'+
 				
 '<button class="w3-button w3-display-left w3-black" onclick="plusDivs(-1)">&#10094;</button>'+
