081d60b79b215d9e9efc67289262def86ae01d05
Hananiel Setiawan <hananiel_s@live.com>
2018-07-17 10:11:16 -0400

    footer modif

--- Samei/foot_script.js
+++ Samei/foot_script.js
@@ -6,16 +6,16 @@ document.getElementById("footerPage").innerHTML =
   '<tbody>'+
     '<tr>'+
 		'<td width="17%"><a href="https://duke.edu/" target="_blank" rel="noopener"><img src="image/duke_logo.png" width=110px alt="Duke Logo"/></a></td>'+
-      '<td width="30%"><pfoot>© Copyright 2018 Duke University</pfoot><br>'+
+      '<td width="32%"><pfoot>© Copyright 2018 Duke University</pfoot><br>'+
         '<pfoot>The Samei Group</pfoot></td>'+
-      '<td width="37%">'+
-		'<pfoot>Ravins Advanced Imaging Laboratory</pfoot><br>'+
-		'<pfoot>Hock Plaza, Suite 302</pfoot><br>'+
-		'<pfoot>2424 Erwin Road, Durham, NC 27705</pfoot><td width="0%"></td>'+
+      '<td width="35%">'+
+		'<pfoot>RAI Laboratories</pfoot><br>'+
+		'<pfoot>Hock Plaza, Ste 302</pfoot><br>'+
+		'<pfoot>2424 Erwin Rd, Durham, NC 27705</pfoot><td width="0%"></td>'+
       '<td width="16%">'+
 		'<pfoot><em>Phone</em>: +1 (919) 684-1440</pfoot><br>'+
 		'<pfoot><em>Fax</em>: +1 (919) 684-1491</pfoot><br>'+
-		'<pfoot><a href="mailto:robin.kearney@dm.duke.edu" target="_blank" rel="noopener">robin.kearney@dm.duke.edu</a></pfoot><td width="0%">'+
+		'<pfoot><a href="mailto:robin.kearney@dm.duke.edu" target="_blank" rel="noopener" style="text-decoration: none; color: white">robin.kearney@dm.duke.edu</a></pfoot><td width="0%">'+
 			'</td>'+
     '</tr>'+
   '</tbody>'+
